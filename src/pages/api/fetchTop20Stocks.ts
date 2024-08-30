ecaf8431a9d05c5ce66f029d8ffc466d271173ba
JoycexChan <joyce770109@gmail.com>
2024-08-30 08:55:01 +0800

    排名前二十2

--- src/pages/api/fetchTop20Stocks.ts
+++ src/pages/api/fetchTop20Stocks.ts
@@ -5,18 +5,24 @@ import firebaseAdmin from '../../firebaseAdmin';
 const db = firebaseAdmin.firestore();
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    const top20Ref = db.collection('Top20');
-    const querySnapshot = await top20Ref
-      .orderBy('pb_ratio', 'asc') // 首先按股價淨值比由小到大排序
-      .orderBy('f_score', 'desc') // 如果股價淨值比相同，則按 F 分數由高到低排序
-      .limit(20)
-      .get();
-    const stocks = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-
-    res.status(200).json(stocks);
-  } catch (error) {
-    console.error("Error fetching data from Firestore:", error);
-    res.status(500).json({ error: 'Error fetching data' });
-  }
-};
+    try {
+      const top20Ref = db.collection('Top20');
+      const querySnapshot = await top20Ref
+        .orderBy('pb_ratio', 'asc')
+        .orderBy('f_score', 'desc')
+        .limit(20)
+        .get();
+      const stocks = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+  
+      res.status(200).json(stocks);
+    } catch (error) {
+      console.error("Error fetching data from Firestore:", error);
+      // 使用類型斷言來處理錯誤
+      if (error instanceof Error) {
+        res.status(500).json({ error: 'Internal server error', details: error.message });
+      } else {
+        res.status(500).json({ error: 'Internal server error', details: 'Unknown error' });
+      }
+    }
+  };
+  
