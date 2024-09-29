79ccd8e63de556282b5e262c749a559c9490ba71
JoycexChan <163320736+JoycexChan@users.noreply.github.com>
2024-09-29 11:48:00 +0800

    Delete src/components/LoadingIcon.tsx

--- src/components/LoadingIcon.tsx
+++
@@ -1,9 +0,0 @@
-import styles from '../styles/StockSummary.module.css';
-
-const LoadingIcon = () => (
-  <div className={styles.loadingAnimation}>
-    <div className={styles.spinner}></div> {/* 加載旋轉圖標 */}
-  </div>
-);
-
-export default LoadingIcon;
